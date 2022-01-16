8961f24d0d383931022dc8729bb67a0da05152a0
Ryan <86175804+marconiryan@users.noreply.github.com>
2022-01-16 01:19:33 -0300

    ...
    esqueci de atualizar o keyboard

--- src/Main/Keyboard.java
+++ src/Main/Keyboard.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
-    public boolean keyDown = false,keyUp = false, keyRight = false, keyLeft = false, keySpeed= false, keyRestart= false;
+    public boolean keyRight = false, keyLeft = false, keySpeed= false, keyRestart= false;
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -14,8 +14,6 @@ public class Keyboard implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
-        if (code == KeyEvent.VK_W){keyUp = true;}
-        if (code == KeyEvent.VK_S){keyDown = true;}
         if (code == KeyEvent.VK_A){keyLeft = true;}
         if (code == KeyEvent.VK_D){keyRight = true;}
         if (code == KeyEvent.VK_SPACE){keySpeed = true;}
@@ -26,8 +24,6 @@ public class Keyboard implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
-        if (code == KeyEvent.VK_W){keyUp = false;}
-        if (code == KeyEvent.VK_S){keyDown = false;}
         if (code == KeyEvent.VK_A){keyLeft = false;}
         if (code == KeyEvent.VK_D){keyRight = false;}
         if (code == KeyEvent.VK_SPACE){keySpeed = false;}
